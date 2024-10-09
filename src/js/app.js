6167bb80be3fa5fdfd68cf9583ecbc3813035a4f
Orangel Hernandez <orangel.hernandez1999@gmail.com>
2024-10-09 19:33:57 +0000

    añadidas ternarias condicionales

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { left } from "@popperjs/core";
 import "../style/index.css";
 
 /**
@@ -32,15 +33,31 @@ function render(variables = {}) {
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <img src=" https://media.licdn.com/dms/image/v2/D4E0BAQEcO3fgdcSiEg/company-logo_200_200/company-logo_200_200/0/1688681342958/4geeksacademyes_logo?e=2147483647&v=beta&t=kGELw1vfpMAZy4w_xBhs1Hz5SYKBO35m7NI9bo3E2Sg" class="photo" />
+          <h1>${variables.name ? variables.name : "Orangel"} ${
+    variables.lastName ? variables.lastName : "Hernandez"
+  } </h1>
+          <h2>${variables.role ? variables.role : "Traductor"}</h2>
+          <h3>${variables.city ? variables.city : "Maracaibo"}, ${
+    variables.country ? variables.country : "Venezuela"
+  }</h3>
+          <ul class="${
+            variables.socialMediaPosition === "position-right"
+              ? "position-right"
+              : "position-left"
+          }">
+            <li><a href="https://twitter.com/${
+              variables.twitter ? variables.twitter : "@elonmusk"
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : "OrangelH27"
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${
+              variables.linkedin ? variables.linkedin : "alesanchezr"
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram ? variables.instagram : "orangel_hernandez"
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
